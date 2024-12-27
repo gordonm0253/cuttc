88d1f39f3d21718381bac6b278c6e395e3d0a650
gordonm0253 <gordonm0253@gmail.com>
2024-12-27 14:56:37 -0500

    board page layout complete

--- src/board.jsx
+++ src/board.jsx
@@ -1,19 +1,85 @@
 import React from "react";
 import malong from "./assets/malong.jpeg";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
+import './App.css'; 
+
 
 export default function Board() {
     return (
         <>
-            <h1>2024-2025 E-Board</h1>
-            <Profile 
-                name = "Ma Long"
-                position = "Grandmaster"
-                netid = "ml2239"
-                image = {malong}
-                major = {["Computer Science"]}
-                year = "25"
-                
-            />
+            <div className = "pageTitle">
+                <h1>2024-2025 E-Board</h1>
+            </div>
+            <Container fluid="lg">
+                <Row>
+                    <Col>
+                        <Profile
+                            name = "Ma Long"
+                            position = "Supreme Leader"
+                            netid = "ml44"
+                            bioimage = {malong}
+                            major = {["Biology", "History"]}
+                            year = "26"
+                        />
+                    </Col>
+                    <Col>
+                        <Profile
+                            name = "Ma Long"
+                            position = "Supreme Leader"
+                            netid = "ml44"
+                            bioimage = {malong}
+                            major = {["Electrical Engineering"]}
+                            year = "27"
+                        />
+                    </Col>
+                    <Col>
+                        <Profile
+                            name = "Ma Long"
+                            position = "Supreme Leader"
+                            netid = "ml44"
+                            bioimage = {malong}
+                            major = {["Computer Science", "Math"]}
+                            year = "28"
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Profile
+                            name = "Ma Long"
+                            position = "Supreme Leader"
+                            netid = "ml44"
+                            bioimage = {malong}
+                            major = {["Biology", "History"]}
+                            year = "26"
+                        />
+                    </Col>
+                    <Col>
+                        <Profile
+                            name = "Ma Long"
+                            position = "Supreme Leader"
+                            netid = "ml44"
+                            bioimage = {malong}
+                            major = {["Electrical Engineering"]}
+                            year = "27"
+                        />
+                    </Col>
+                    <Col>
+                        <Profile
+                            name = "Ma Long"
+                            position = "Supreme Leader"
+                            netid = "ml44"
+                            bioimage = {malong}
+                            major = {["Computer Science", "Math"]}
+                            year = "28"
+                        />
+                    </Col>
+                </Row>
+            </Container>
         </>
         
     );
@@ -21,10 +87,19 @@ export default function Board() {
 
 
 
-function Profile({info}) {
+function Profile({name, position, netid, bioimage, major, year}) {
+    function getMajor(majorList) {
+        return majorList.join(", ");
+    }
     return (
-        <div>
-
+        <div className = "profileDiv">
+            <img src = {bioimage} className = "bioImages"></img>
+            <div className = "bioText">
+                <p>{name} '{year}</p>
+                <p>{position}</p>
+                <p>Email: {netid}@cornell.edu</p>
+                <p>Major(s): {getMajor(major)}</p>
+            </div>
         </div>
     );
 }
